c592ed15376d6a5efba03b940c4ab853ea2d296f
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-08-22 12:04:46 +0200

    Use pinned cake version

--- src/Test/Properties/AssemblyInfo.cs
+++ src/Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b2adfe7d-ea74-4d5d-8cc3-505b0955afb2")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
